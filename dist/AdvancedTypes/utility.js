1aa509df8b36f20f72a4578e8ec05a93ecf35a46
Amandeep <amanr5413@gmail.com>
2025-01-21 13:47:28 +0530

    [Aman] ADD : Adding New Files

--- dist/AdvancedTypes/utility.js
+++ dist/AdvancedTypes/utility.js
@@ -7,3 +7,22 @@ let user3 = { name: "aman", age: 21, pin: 1740 }; //no modification
 let user4 = { name: "amandeep" }; //only picked 
 let user5 = { name: "aman", age: 21 }; //exclude 
 let user6 = { name: "aman", age: 21, pin: 1740, add: "una" }; //optional are also include
+const rolePermissions = {
+    admin: true,
+    user: true,
+    guest: false,
+};
+function getUser() {
+    return { id: 1, name: "Alice" };
+}
+const userr = {
+    id: 1,
+    name: "Alice",
+};
+class User111 {
+    constructor() {
+        this.name = "Alice";
+        this.age = 30;
+    }
+}
+const user111 = new User111();
